733c0e2a53cb9dbfe4aeb20eda6b72d018181c5e
rechard chen <rechardchen123@outlook.com>
2018-10-17 13:45:16 +0100

    add some dependencies

--- install new tensorflow in Linux environment.sh
+++ install new tensorflow in Linux environment.sh
@@ -10,4 +10,11 @@ pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensor
 # or below if you want gpu, support, but cuda and cudnn are required, see docs for more install instructions
 pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.11.0-cp36-cp36m-linux_x86_64.whl
 
+#if there are some problems when activate the tensorflow, you may need some dependencies to install.
+sudo yum install python-dev python-pip  # or python3-dev python3-pip
+
+#Install the TensorFlow pip package dependencies (if using a virtual environment, omit the --user argument):
+pip install -U pip six numpy wheel mock
+pip install -U keras_applications==1.0.5 --no-deps
+pip install -U keras_preprocessing==1.0.3 --no-deps
 
